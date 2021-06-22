16432cf48ae65c71ee1c16e25ca641b9a2c56be3
Beom-Chu <chorocsky@gmail.com>
2021-06-22 14:31:45 +0900

    재시도

--- algorithm/programers/Carpet.java
+++ algorithm/programers/Carpet.java
@@ -14,33 +14,50 @@ Leo가 본 카펫에서 갈색 격자의 수 brown, 노란색 격자의 수 yell
 노란색 격자의 수 yellow는 1 이상 2,000,000 이하인 자연수입니다.
 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.*/
 public class Carpet {
-	
-	public int[] solution(int brown, int yellow) {
-		
-		int[] rtn = new int[2];
-		
-		int yellHV = (brown/2)-2;	//노란 부분 가로세로 합
-		
-		for(int H=yellHV-1 , V=1; H>0; H--, V++) {
-			if(H*V == yellow) {	//노란 가로*세로 == 노랑 넓이 체크
-				rtn[0]=H+2;		//가로(노란가로+갈색가로)
-				rtn[1]=V+2;		//세로(노란세로+갈색세로)
-				break;
-			}
-		}
-		return rtn;
-	}
-	
-	public static void main(String[] args) {
-		
-		int[] colors = {10,	2};		/*[4, 3]*/
-//		int[] colors = {8,	1 };	/*[3, 3]*/
-//		int[] colors = {24,	24};	/*[8, 6]*/
-		
-		Carpet carpet = new Carpet();
-		int[] rtn = carpet.solution(colors[0], colors[1]);
-		
-		System.out.printf("[%s , %s]", rtn[0],rtn[1]);
-	}
+
+  /* 재시도 */
+  public int[] solution2(int brown, int yellow) {
+    
+    int hw = (brown / 2) - 2;
+    int h = 1, w = hw - h;
+
+    for (; h < hw; h++, w--) {
+
+      if (h * w == yellow) {
+        break;
+      }
+    }
+
+    return new int[] {w + 2, h + 2};
+  }
+  
+  
+  public int[] solution(int brown, int yellow) {
+
+    int[] rtn = new int[2];
+
+    int yellHV = (brown / 2) - 2; // 노란 부분 가로세로 합
+
+    for (int H = yellHV - 1, V = 1; H > 0; H--, V++) {
+      if (H * V == yellow) { // 노란 가로*세로 == 노랑 넓이 체크
+        rtn[0] = H + 2; // 가로(노란가로+갈색가로)
+        rtn[1] = V + 2; // 세로(노란세로+갈색세로)
+        break;
+      }
+    }
+    return rtn;
+  }
+
+  public static void main(String[] args) {
+
+    int[] colors = {10, 2}; /* [4, 3] */
+//     int[] colors = {8, 1 }; /*[3, 3]*/
+//     int[] colors = {24, 24}; /*[8, 6]*/
+
+    Carpet carpet = new Carpet();
+    int[] rtn = carpet.solution2(colors[0], colors[1]);
+
+    System.out.printf("[%s , %s]", rtn[0], rtn[1]);
+  }
 
 }
