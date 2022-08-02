a3f5659c0316ffe021f9f42928929d42d2ff6091
bumsoo <chorocsky@gmail.com>
2022-08-02 20:15:19 +0900

    집합 ing... 메모리 초과

---
+++ algorithm/baekjoon/Set.java
@@ -0,0 +1,124 @@
+/*
+집합 [11723]
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1.5 초	4 MB (하단 참고)	57989	17839	12632	29.274%
+문제
+비어있는 공집합 S가 주어졌을 때, 아래 연산을 수행하는 프로그램을 작성하시오.
+
+add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
+remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
+check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
+toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
+all: S를 {1, 2, ..., 20} 으로 바꾼다.
+empty: S를 공집합으로 바꾼다.
+입력
+첫째 줄에 수행해야 하는 연산의 수 M (1 ≤ M ≤ 3,000,000)이 주어진다.
+
+둘째 줄부터 M개의 줄에 수행해야 하는 연산이 한 줄에 하나씩 주어진다.
+
+출력
+check 연산이 주어질때마다, 결과를 출력한다.
+
+예제 입력 1
+26
+add 1
+add 2
+check 1
+check 2
+check 3
+remove 2
+check 1
+check 2
+toggle 3
+check 1
+check 2
+check 3
+check 4
+all
+check 10
+check 20
+toggle 10
+remove 20
+check 10
+check 20
+empty
+check 1
+toggle 1
+check 1
+toggle 1
+check 1
+예제 출력 1
+1
+1
+0
+1
+0
+1
+0
+1
+0
+1
+1
+0
+0
+0
+1
+0
+
+비트마스킹
+ */
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Set {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(reader.readLine());
+        List<Calculation> cal = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            cal.add(new Calculation(reader.readLine()));
+        }
+
+        // TODO : 메모리 초과...
+
+        int set = 0;
+
+        for(Calculation c : cal) {
+            if(c.action.equals("add")) {
+                set |= (1 << c.no);
+            } else if(c.action.equals("remove")) {
+                set &= ~(1 << c.no);
+            } else if(c.action.equals("toggle")) {
+                set ^= (1 << c.no);
+            } else if(c.action.equals("all")) {
+                set = (1 << 21) - 1;
+            } else if(c.action.equals("empty")) {
+                set = 0;
+            } else if(c.action.equals("check")) {
+                System.out.println((set & (1 << c.no)) == 0 ? 0 : 1);
+            }
+        }
+    }
+
+    static class Calculation {
+        String action;
+        int no;
+
+        Calculation(String s) {
+            StringTokenizer st = new StringTokenizer(s);
+            action = st.nextToken();
+            if(!action.equals("all") && !action.equals("empty")) {
+                no = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+}
